5678be0297cdd046e46ef14524c4e8b155c68cdd
giantpro1 <shakiruolawale597@gmail.com>
2024-01-11 00:26:33 +0100

    images

--- demo/lib/main.dart
+++ demo/lib/main.dart
@@ -22,57 +22,73 @@ class _MyAppState extends State<MyApp> {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        appBar: AppBar(
-          centerTitle: true,
-          title: const Text('Welcome Arike Preorder'),
+      home: Container(
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: NetworkImage(
+                'https://img.freepik.com/free-photo/space-background-realistic-starry-night-cosmos-shining-stars-milky-way-stardust-color-galaxy_1258-154643.jpg'),
+            fit: BoxFit.cover,
+          ),
         ),
-        body: Column(
-          mainAxisAlignment: nums == 0
-              ? MainAxisAlignment.start
-              : nums == 1
-                  ? MainAxisAlignment.center
-                  : MainAxisAlignment.end,
-          children: [
-            Container(
-              height: 30,
-              child: Row(
-                mainAxisAlignment: nums == 0
-                    ? MainAxisAlignment.start
-                    : nums == 1
-                        ? MainAxisAlignment.center
-                        : MainAxisAlignment.end,
-                children: [
-                  ElevatedButton(
-                      style: style,
-                      onPressed: () {
-                        setState(() {
-                          // myNewButton = !myNewButton;
-                          nums == 2 ? nums = 0 : nums = nums + 1;
-                        });
-                      },
-                      child: const Text(
-                        "My Button",
-                        style: TextStyle(color: Colors.white),
-                      )),
-                  // Text(myNewButton ? "This Is true" : myNewText),
-                  // Text("My Student"),
-                  // Text("My Student"),
-                ],
-              ),
-            )
-          ],
+        child: Scaffold(
+          appBar: AppBar(
+            centerTitle: true,
+            title: const Text('Welcome Arike Preorder'),
+          ),
+          body: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: nums == 0
+                  ? MainAxisAlignment.start
+                  : nums == 1
+                      ? MainAxisAlignment.center
+                      : MainAxisAlignment.end,
+              children: [
+                Container(
+                  height: 40,
+                  child: Image.network(
+                      'https://img.freepik.com/free-photo/space-background-realistic-starry-night-cosmos-shining-stars-milky-way-stardust-color-galaxy_1258-154643.jpg'),
+                ),
+                Container(
+                  height: 30,
+                  child: Row(
+                    mainAxisAlignment: nums == 0
+                        ? MainAxisAlignment.start
+                        : nums == 1
+                            ? MainAxisAlignment.center
+                            : MainAxisAlignment.end,
+                    children: [
+                      ElevatedButton(
+                          style: style,
+                          onPressed: () {
+                            setState(() {
+                              // myNewButton = !myNewButton;
+                              nums == 2 ? nums = 0 : nums = nums + 1;
+                            });
+                          },
+                          child: const Text(
+                            "My Button",
+                            style: TextStyle(color: Colors.white),
+                          )),
+                      // Text(myNewButton ? "This Is true" : myNewText),
+                      // Text("My Student"),
+                      // Text("My Student"),
+                    ],
+                  ),
+                )
+              ],
+            ),
+          ),
+          bottomNavigationBar: BottomAppBar(
+              color: Colors.amberAccent,
+              child: IconButton(
+                  onPressed: () {
+                    // print("Button Pressed");
+                  },
+                  icon: const Icon(
+                    Icons.home,
+                    color: Colors.white,
+                  ))),
         ),
-        bottomNavigationBar: BottomAppBar(
-            color: Colors.amberAccent,
-            child: IconButton(
-                onPressed: () {
-                  // print("Button Pressed");
-                },
-                icon: const Icon(
-                  Icons.home,
-                  color: Colors.white,
-                ))),
       ),
     );
   }
