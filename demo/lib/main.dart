b5faf6e317597105bcb6654b54a70b7e046ff3b7
giantpro1 <shakiruolawale597@gmail.com>
2024-01-10 01:17:28 +0100

    working button

--- demo/lib/main.dart
+++ demo/lib/main.dart
@@ -4,9 +4,15 @@ void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  // const MyApp({super.key});
+class MyApp extends StatefulWidget {
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
 
+class _MyAppState extends State<MyApp> {
+  // const MyApp({super.key});
+  bool myNewButton = false;
+  String myNewText = "Hello";
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -15,13 +21,39 @@ class MyApp extends StatelessWidget {
           centerTitle: true,
           title: const Text('Welcome Arike Preorder'),
         ),
+        body: Column(
+          // crossAxisAlignment: CrossAxisAlignment.start,
+          // mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextButton(
+                onPressed: () {
+                  setState(() {
+                    myNewButton = !myNewButton;
+                  });
+                },
+                child: const Text("My Button")),
+            Container(
+              height: 30,
+              child: Row(
+                children: [
+                  Text(myNewButton ? "This Is true" : myNewText),
+                  // Text("My Student"),
+                  // Text("My Student"),
+                ],
+              ),
+            )
+          ],
+        ),
         bottomNavigationBar: BottomAppBar(
             color: Colors.amberAccent,
             child: IconButton(
                 onPressed: () {
                   // print("Button Pressed");
                 },
-                icon: const Icon(Icons.home, color: Colors.white,))),
+                icon: const Icon(
+                  Icons.home,
+                  color: Colors.white,
+                ))),
       ),
     );
   }
