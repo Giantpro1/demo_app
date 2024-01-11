822e180e08f992b8984d262db0aac99758ceadd9
giantpro1 <shakiruolawale597@gmail.com>
2024-01-11 14:46:52 +0100

    cg

--- demo/lib/home_page.dart
+++ demo/lib/home_page.dart
@@ -26,8 +26,7 @@ class _MyScalfordAppState extends State<MyScalfordApp> {
           //         : MainAxisAlignment.end,
           children: [
             Container(
-              child: Image.network(
-                  'https://img.freepik.com/free-photo/space-background-realistic-starry-night-cosmos-shining-stars-milky-way-stardust-color-galaxy_1258-154643.jpg'),
+              child: Image.asset('images/pixel.jpg'),
             ),
             // Container(
             //   height: 30,
